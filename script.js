ebda5162ddc746d2a368e57132320610ea787f01
IrvynCornejo <irvyncornejo@gmail.com>
2019-02-11 11:15:16 -0600

    Se añadió el archivo, para pedir ingresar datos

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 
 
-  // Or with jQuery
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
+  var instance = M.Carousel.init({
+fullWidth: true,
+indicators: true
+  });
+  var instance = M.Carousel.init({
+    fullWidth: true
+  });
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.carousel');
+      var instances = M.Carousel.init(elems);
+    });
+    document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.materialboxed');
+var instances = M.Materialbox.init(elems);
+});
